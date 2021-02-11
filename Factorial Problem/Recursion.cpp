f848cf1bebdbe9dd04f112783aa4007caa395689
Rhea Sidana <37862762+RheaSidana@users.noreply.github.com>
2021-02-11 18:25:32 +0530

    Create Recursion.cpp

---
+++ Factorial Problem/Recursion.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int fact(int n){
+    if(n==1){
+        return n;
+    }
+    else{
+        int r=n*fact(n-1);
+        //cout<<endl<<n<<" "<<(n-1)<<":"<<r;
+        return r;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    int r=fact(n);
+    cout<<endl<<r;
+    return 0;
+}
+
+/*output:
+>>>4
+
+24
+
+*/
