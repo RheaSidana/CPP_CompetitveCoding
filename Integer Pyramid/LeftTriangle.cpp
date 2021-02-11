38cb80ccaa61c81d13232bb533a65c54d88e221e
Rhea Sidana <37862762+RheaSidana@users.noreply.github.com>
2021-02-11 17:43:23 +0530

    Create LeftTriangle.cpp

---
+++ Integer Pyramid/LeftTriangle.cpp
@@ -0,0 +1,29 @@
+/*
+take an integer input and then draw the pattern according to it. Say for example if you enter 5 then, the pattern should be like this-
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5 
+
+Input Format
+You will take an integer input n from stdin.
+
+Constraints
+1 <= n <= 1000
+*/
+
+#include <iostream>
+using namespace std;
+int main(int argc, char *a[])
+{
+    int n,i,j;
+    cin>>n;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            cout<<j;
+            if(j<i)cout<<" ";
+        }
+        if(i<n)cout<<endl;
+    }
+}
