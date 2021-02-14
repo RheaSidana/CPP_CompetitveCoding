3e9261fddfd842a1dbfa5197e864c05e00691054
Rhea Sidana <37862762+RheaSidana@users.noreply.github.com>
2021-02-14 15:08:45 +0530

    Create AscRepeatedRowNoRightTriangle.cpp

---
+++ Integer Pyramid/AscRepeatedRowNoRightTriangle.cpp
@@ -0,0 +1,34 @@
+/*
+take an integer input and then draw the pattern according to it. Say for example if you enter 5 then, the pattern should be like this-
+
+        1
+      2 2
+    3 3 3
+  4 4 4 4
+5 5 5 5 5 
+
+Input Format
+You will take an integer input n from stdin.
+
+Constraints
+1 <= n <= 1000
+*/
+
+#include <iostream>
+using namespace std;
+int main(int argc, char *a[])
+{
+    int n,i,j;
+    cin>>n;
+    for(i=1;i<=n;i++){
+        for(j=i;j<n;j++){
+            cout<<"  ";
+        }
+        for(j=1;j<=i;j++){
+            cout<<i;
+            if(j<i)cout<<" ";
+        }
+        if(i<n)cout<<endl;
+    }
+    return 0;
+}
