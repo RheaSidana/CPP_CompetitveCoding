0b5b3f9a6382d91717d5e4825eec603a1bbde60f
Rhea Sidana <37862762+RheaSidana@users.noreply.github.com>
2021-02-19 23:41:17 +0530

    Create note.cpp

---
+++ Algorithm Problems/Kate and note/note.cpp
@@ -0,0 +1,29 @@
+/*
+
+Vasya decided to pass a very large integer n to Kate. First, he wrote that number as a string, then he appended to the right integer k the number of digits in n.
+
+Magically, all the numbers were shuffled in arbitrary order while this note was passed to Kate. The only thing that Vasya remembers, is a non-empty substring of n (a substring of n is a sequence of consecutive digits of the number n).
+Vasya knows that there may be more than one way to restore the number n. Your task is to find the smallest possible initial integer n. Note that decimal representation of number n contained no leading zeroes, except the case the integer n was equal to zero itself (in this case a single digit 0 was used).
+
+
+Input Format
+You will be given a function with two strings as arguments where number of digits in first string does not exceed and the second string can contain leading zeroes.
+
+
+Constraints
+1 < |S| < 10^5
+
+Output Format
+Return the smallest integer n which Vasya could pass to Kate.
+
+
+Sample TestCase 1
+Input
+003512
+021
+Output
+30021
+
+*/
+
+
