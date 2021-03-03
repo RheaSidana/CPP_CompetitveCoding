361e99090395c92fd9a219d6afa5c30fcb27d1ea
Rhea Sidana <37862762+RheaSidana@users.noreply.github.com>
2021-03-03 20:14:40 +0530

    Update note.cpp

--- Algorithm Problems/Kate and note/note.cpp
+++ Algorithm Problems/Kate and note/note.cpp
@@ -32,7 +32,7 @@ Output
 3769
 
 Sample TestCase 3
-Input 
+Input
 399876729
 396
 Output
@@ -45,6 +45,19 @@ Input
 Output
 23869
 
+*/
+
+/*
+Algorithm : 
+step 1: sort the magical number in ascending number 
+step 2: remove length of the original number from the magical number
+step 3: remove the remember number "substring" 
+step 4: check if both magic number and remember should not contain all zero
+            if allzero 
+               ans = 0
+step 4: assign first number (number should be non zero)
+step 5: compare the numbers from sorted list to the remember list 
+step 6: if remeber list is greater than the sorted list numbers then assign remember in the end 
 */
 
 #include <iostream>
@@ -62,12 +75,12 @@ class MagicalNumber {
     // r is the list of numbers rememered by the sender 
     // res is the resultant list/number 
     list<int> num, r, res;
-    
+
 
     //remove the length of the number that was appended before sending 
+    //len is the length of the original number sent 
     void removeStringLength(int len) {
-        int strlen = len;
-        while (len!=0) {
+        while (len != 0) {
             int x = len % 10;
             list<int> ::iterator it;
             for (it = num.begin(); it != num.end(); it++) {
@@ -82,18 +95,24 @@ class MagicalNumber {
 
     //assigning the num list the jumbled numbers
     void assignNumber() {
+        //step 1: find the actual length of the number sent
         int len = no.length();
         int temp;
         for (int i = 0; i < len; i++) {
             temp = int(no[i]) - int('0');
             num.push_back(temp);
         }
-        len--;
+        if (len > 10)len -= 2;
+        else len--;
         //cout <<"\n Length: "<< len<<endl;
+
+        //step 2: sort the numbers in ascending order 
         num.sort();
         //cout << "Displaying the num list: ";
         //printNum();
         //cout << "\nRemoving the size of the string : ";
+
+        //step 3: remove the length of the string from the list
         removeStringLength(len);
         //cout << "Displaying the num list: ";
         //printNum();
@@ -112,15 +131,18 @@ class MagicalNumber {
         for (int i = 0; rem[i] != '\0'; i++) {
             list<int> ::iterator it;
             int x = int(rem[i]) - int('0');
+            //Step 1: add the remember numbers order to r list
             r.push_back(x);
             for (it = num.begin(); it != num.end(); it++) {
                 if (x == (*it)) {
+                    //Step 2: remove the number from the num list
                     num.erase(it);
                     break;
                 }
             }
         }
     }
+
     /*
     void printR() {
         list<int> ::iterator it;
@@ -137,43 +159,58 @@ class MagicalNumber {
             res.push_back(*p);
         }
     }
-  
+
     //compare the num list value to the initial value of the r list
-    int compareRemember(int val,int *first) {
-        list<int> :: iterator p;
-        p=r.begin();
+    int compareRemember(int val, int* first) {
+        list<int> ::iterator p;
+        p = r.begin();
+        //Step 1: if(val==(*p)) then
+        //step 2: if(r[1] < val) then
+        //Step 3: assign r list numbers 
         if (val == (*p)) {
-            //cout << "\nValue is equal to remember beginning ";
             p++;
             if (val > (*p)) {
-                //cout << "\nValue is equal to remember next to beginning ";
                 assignRemember();
                 return 1;
             }
         }
-        else if (val > (*p) && (*p) != 0 && *first!=-1) {
+        //Step 4: if(val>r[0] && r[0]!=0 && first is assigned) then
+        //Step 5: assign r list numbers
+        else if (val > (*p) && (*p) != 0 && *first != -1) {
             assignRemember();
             return 1;
         }
-        else if (val > (*p) && *first==-1 && (*p) != 0) {
+        //Step 6: if(val>r[0] r[0]!=0 && first is not assigned) then 
+        //Step 7: assign r list number 
+        else if (val > (*p) && *first == -1 && (*p) != 0) {
             assignRemember();
             *first = *p;
             return 1;
         }
         return 0;
     }
-    
+
     //assign the first number of the res list 
     //it must not be zero at the begining 
-    int assignFirst(int *first) {
+    int assignFirst(int* first) {
         int val;
+        //flag checks if the r list number is assigned to res list or not 
         int flag = 0;
+        //allzero checks if the num list only contains zero
+        int allzero = 0;
         list<int>::iterator it;
         for (it = num.begin(); it != num.end(); it++) {
-            if ((*it) == 0)continue;
+            if ((*it) == 0) {
+                allzero = 1;
+                continue;
+            }
             else {
+                allzero = 0;
                 val = *it;
-                flag = compareRemember(val,first);
+                //compare the val with r list 
+                //flag = 1 :  r list numbers are assigned to res list
+                //flag = 0 :  val is assigned to res list
+                flag = compareRemember(val, first);
                 if (flag == 0)
                 {
                     res.push_back(val);
@@ -183,14 +220,19 @@ class MagicalNumber {
                 break;
             }
         }
+        //if num list numbers are all zeros then assign r list to res list 
+        if (allzero) {
+            assignRemember();
+            flag = 1;
+        }
         return flag;
     }
-    
+
     //assigning the rest of the numbers to res list 
-    void assignRest(int flag,int *first) {
+    void assignRest(int flag, int* first) {
         list<int> ::iterator it;
         int val;
-        //cout << "\nFlag = " << flag;
+        //flag=1 : r list numbers are already assigned 
         if (flag == 1) {
             for (it = num.begin(); it != num.end(); it++) {
                 val = *it;
@@ -198,18 +240,54 @@ class MagicalNumber {
             }
         }
         else {
-            for(it = num.begin(); it != num.end();it++) {
+            for (it = num.begin(); it != num.end(); it++) {
                 val = *it;
                 if (flag == 0) {
-                    flag = compareRemember(val,first);
+                    flag = compareRemember(val, first);
                     //cout << "\nFlag after remebering: " << flag;
                 }
                 res.push_back(val);
             }
+            //if flag is still 0 
+            //the numbers in the num list are all less than the r[0]
+            //then only r list numbers are left to be assigned to res list
+            // assign r list numbers to res 
             if (flag == 0) {
                 assignRemember();
             }
         }
+    }
+
+    //check if all numbers are zero in num and r list
+    int checkIfAllZero() {
+        list<int> ::iterator it;
+        int flag = 0;
+        //checks in num list first
+        for (it = num.begin(); it != num.end(); it++) {
+            if (*it == 0) {
+                flag = 1;
+            }
+            else {
+                flag = 0;
+                break;
+            }
+        }
+        //flaag=1: num list contains all zero
+        //then check in res list 
+        if (flag != 0) {
+            for (it = r.begin(); it != r.end(); it++) {
+                if (*it == 0) {
+                    flag = 1;
+                }
+                else {
+                    flag = 0;
+                    break;
+                }
+            }
+        }
+        //if both are all zeros then this if is executed 
+        if (flag != 0)return 1;
+        return 0;
     }
 
 public:
@@ -228,24 +306,32 @@ public:
         //cout << "\nDisplaying the r list: ";
         //printR();
     }
-    
+
     //display the number in the res list 
     void display() {
         //cout << "\nDisplaying the result: \n";
         list<int> ::iterator it;
         for (it = res.begin(); it != res.end(); it++) {
-            cout << *it << " ";
+            cout << *it;
         }
     }
-    
+
     //creating the number using num and r list 
     void createNumber() {
+        //check for all zeros then the res is zero
+        int allZero = checkIfAllZero();
+        if (allZero) {
+            res.push_back(0);
+            return;
+        }
         int first = -1;
         //cout << "\nAssigning the first number: ";
         int flag = assignFirst(&first);
         //cout << "\nDisplaying the num list: ";
         //printNum();
         //display();
+
+        //assigning the rest of the numbers in the res list
         assignRest(flag, &first);
     }
 };
