1df86e5a25be70a28cbcd76ab6ab3f04ba2e3828
Rhea Sidana <37862762+RheaSidana@users.noreply.github.com>
2021-03-03 18:10:19 +0530

    Update note.cpp

--- Algorithm Problems/Kate and note/note.cpp
+++ Algorithm Problems/Kate and note/note.cpp
@@ -24,6 +24,239 @@ Input
 Output
 30021
 
+Sample TestCase 2
+Input
+79436
+37
+Output
+3769
+
+Sample TestCase 3
+Input 
+399876729
+396
+Output
+23967799
+
+Sample TestCase 4
+Input
+365289
+38
+Output
+23869
+
 */
+
+#include <iostream>
+#include <string>
+#include <list>
+#include <iterator>
+using namespace std;
+
+//class to identify the actual number sent 
+class MagicalNumber {
+    //no is the jumbled number 
+    //rem is the substring of the remembered number
+    string no, rem;
+    //num is the list of the number
+    // r is the list of numbers rememered by the sender 
+    // res is the resultant list/number 
+    list<int> num, r, res;
+    
+
+    //remove the length of the number that was appended before sending 
+    void removeStringLength(int len) {
+        int strlen = len;
+        while (len!=0) {
+            int x = len % 10;
+            list<int> ::iterator it;
+            for (it = num.begin(); it != num.end(); it++) {
+                if (x == (*it)) {
+                    num.erase(it);
+                    break;
+                }
+            }
+            len = len / 10;
+        }
+    }
+
+    //assigning the num list the jumbled numbers
+    void assignNumber() {
+        int len = no.length();
+        int temp;
+        for (int i = 0; i < len; i++) {
+            temp = int(no[i]) - int('0');
+            num.push_back(temp);
+        }
+        len--;
+        //cout <<"\n Length: "<< len<<endl;
+        num.sort();
+        //cout << "Displaying the num list: ";
+        //printNum();
+        //cout << "\nRemoving the size of the string : ";
+        removeStringLength(len);
+        //cout << "Displaying the num list: ";
+        //printNum();
+    }
+
+    /*
+    void printNum() {
+        list<int> ::iterator it;
+        for (it = num.begin(); it != num.end(); it++) {
+            cout << *it << "  ";
+        }
+    }*/
+
+    //removing the rememered string from num and assigning to the r list
+    void removeRemebernumber() {
+        for (int i = 0; rem[i] != '\0'; i++) {
+            list<int> ::iterator it;
+            int x = int(rem[i]) - int('0');
+            r.push_back(x);
+            for (it = num.begin(); it != num.end(); it++) {
+                if (x == (*it)) {
+                    num.erase(it);
+                    break;
+                }
+            }
+        }
+    }
+    /*
+    void printR() {
+        list<int> ::iterator it;
+        for (it = r.begin(); it != r.end(); it++) {
+            cout << *it << " ";
+        }
+    }
+    */
+
+    //assigning remembered numbers to res list
+    void assignRemember() {
+        list<int> ::iterator p;
+        for (p = r.begin(); p != r.end(); p++) {
+            res.push_back(*p);
+        }
+    }
+  
+    //compare the num list value to the initial value of the r list
+    int compareRemember(int val,int *first) {
+        list<int> :: iterator p;
+        p=r.begin();
+        if (val == (*p)) {
+            //cout << "\nValue is equal to remember beginning ";
+            p++;
+            if (val > (*p)) {
+                //cout << "\nValue is equal to remember next to beginning ";
+                assignRemember();
+                return 1;
+            }
+        }
+        else if (val > (*p) && (*p) != 0 && *first!=-1) {
+            assignRemember();
+            return 1;
+        }
+        else if (val > (*p) && *first==-1 && (*p) != 0) {
+            assignRemember();
+            *first = *p;
+            return 1;
+        }
+        return 0;
+    }
+    
+    //assign the first number of the res list 
+    //it must not be zero at the begining 
+    int assignFirst(int *first) {
+        int val;
+        int flag = 0;
+        list<int>::iterator it;
+        for (it = num.begin(); it != num.end(); it++) {
+            if ((*it) == 0)continue;
+            else {
+                val = *it;
+                flag = compareRemember(val,first);
+                if (flag == 0)
+                {
+                    res.push_back(val);
+                    num.erase(it);
+                    *first = val;
+                }
+                break;
+            }
+        }
+        return flag;
+    }
+    
+    //assigning the rest of the numbers to res list 
+    void assignRest(int flag,int *first) {
+        list<int> ::iterator it;
+        int val;
+        //cout << "\nFlag = " << flag;
+        if (flag == 1) {
+            for (it = num.begin(); it != num.end(); it++) {
+                val = *it;
+                res.push_back(val);
+            }
+        }
+        else {
+            for(it = num.begin(); it != num.end();it++) {
+                val = *it;
+                if (flag == 0) {
+                    flag = compareRemember(val,first);
+                    //cout << "\nFlag after remebering: " << flag;
+                }
+                res.push_back(val);
+            }
+            if (flag == 0) {
+                assignRemember();
+            }
+        }
+    }
+
+public:
+    //getting the values of the magic number and the remebered string 
+    void getStringNo() {
+        //cout << "Enter string : ";
+        getline(cin, no);
+        //cout << "\nEnter sub-string : ";
+        getline(cin, rem);
+        //cout << "\nAssigning number :\n ";
+        assignNumber();
+        //cout << "\nRemoving remember numbers from the string : ";
+        removeRemebernumber();
+        //cout << "\nDisplaying the num list: ";
+        //printNum();
+        //cout << "\nDisplaying the r list: ";
+        //printR();
+    }
+    
+    //display the number in the res list 
+    void display() {
+        //cout << "\nDisplaying the result: \n";
+        list<int> ::iterator it;
+        for (it = res.begin(); it != res.end(); it++) {
+            cout << *it << " ";
+        }
+    }
+    
+    //creating the number using num and r list 
+    void createNumber() {
+        int first = -1;
+        //cout << "\nAssigning the first number: ";
+        int flag = assignFirst(&first);
+        //cout << "\nDisplaying the num list: ";
+        //printNum();
+        //display();
+        assignRest(flag, &first);
+    }
+};
+
+int main()
+{
+    MagicalNumber no;
+    no.getStringNo();
+    no.createNumber();
+    no.display();
+}
+
 
 
